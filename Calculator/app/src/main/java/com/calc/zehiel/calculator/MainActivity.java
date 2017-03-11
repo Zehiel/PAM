96a8b6782d22936070c3fd9324e11255664ad8ac
Andrzej Borzęcki <Andrzej Borzcki>
2017-03-11 13:54:29 +0100

    Added buttons functionality in menu

--- Calculator/app/src/main/java/com/calc/zehiel/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/calc/zehiel/calculator/MainActivity.java
@@ -1,7 +1,9 @@
 package com.calc.zehiel.calculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gotoSimpleCalcActivity(View view){
+        Intent intent = new Intent(this,SimpleCalcActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void gotoAdvancedCalcActivity(View view){
+        Intent intent = new Intent(this,AdvancedCalcActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void gotoAboutActivity(View view){
+        Intent intent = new Intent(this,AboutActivity.class);
+        this.startActivity(intent);
+    }
+
+    public void quitApp(View view){
+        this.finishAffinity();
     }
 }
